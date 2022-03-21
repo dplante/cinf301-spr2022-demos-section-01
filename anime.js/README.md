9139f973cec680a19ffe88eb2bb95428e14129fe
Ismail-at <70117491+Ismail-at@users.noreply.github.com>
2022-03-21 15:10:09 -0400

    Anime.js Demo

---
+++ anime.js/README.md
@@ -0,0 +1,23 @@
+## Ismail Althubyani
+
+## Class: Web Application Development
+
+## Section 01
+
+## My Demo (Anime.js)
+
+## 3/9/2022
+
+## Refrences:
+
+## https://animejs.com/
+
+## https://www.sitepoint.com/get-started-anime-js/
+
+## https://gist.github.com/brickbones/2cc0967dbf753c4ac78f88e427b03d19
+
+## https://www.youtube.com/watch?v=uRDLFXxihgc&t=1s&ab_channel=RedStapler
+
+## cool Demos and examples:
+
+## https://github.com/juliangarnier/anime
---
+++ anime.js/main.js
@@ -0,0 +1,96 @@
+
+// Youtube anime//
+
+anime({
+    targets: '.youtube path',
+    strokeDashoffset: [anime.setDashoffset, 0],
+    easing: 'easeInOutSine',
+    duration: 1500,
+    direction: 'alternate',
+    loop: true
+});
+
+
+
+//boxes anime//
+
+
+
+
+// anime({
+//   targets: 'div.box.red',
+//   translateY: [
+//     { value: 200, duration: 500 },
+//     { value: 0, duration: 800 }
+//   ],
+//   rotate:{
+//     value: '1turn',
+//     easing: 'easeInOutSine'
+//   }
+// });
+
+// anime({
+//   targets: 'div.box.blue',
+//   translateY: [
+//     { value: 200, duration: 500, delay: 1000},
+//     { value: 0, duration: 800 }
+//   ],
+//   rotate:{
+//     value: '1turn',
+//     easing: 'easeInOutSine',
+//     delay: 1000
+//   }
+// });
+
+// anime({
+//   targets: 'div.box.green',
+//   translateY: [
+//     { value: 200, duration: 500, delay: 2000},
+//     { value: 0, duration: 800 }
+//   ],
+//   rotate:{
+//     value: '1turn',
+//     easing: 'easeInOutSine',
+//     delay: 2000
+//   }
+// });
+
+// anime({
+//   targets: 'div.box.yellow',
+//   translateY: [
+//     { value: 200, duration: 500, delay: 3000},
+//     { value: 0, duration: 800 }
+//   ],
+//   rotate:{
+//     value: '1turn',
+//     easing: 'easeInOutSine',
+//     delay: 3000
+//   }
+// });
+
+
+
+
+//loop boxes anime//
+
+
+
+
+var playPause = anime({
+    targets: 'div.box',
+    translateY: [
+      { value: 200, duration: 500 },
+      { value: 0, duration: 800 }
+    ],
+    rotate:{
+      value: '1turn',
+      easing: 'easeInOutSine'
+    },
+    delay: function(el, i, l){ return i * 1000},
+    autoplay: false,
+    loop:true
+  });
+  
+  document.querySelector('#boxes .play').onclick = playPause.play;
+  document.querySelector('#boxes .pause').onclick = playPause.pause;
+
